d8d7486dd5c64b79b50274dba62c1558f500a355
Jackson-Miguel <jacksonmiguelcarvalho51@gmail.com>
2025-05-16 11:05:10 -0300

    Fudeu

--- index.js
+++ index.js
@@ -100,10 +100,13 @@ function add_porcent(){
 }
 function add_vir(){
     if(tela.value){
-        tela.value += tela.innerText + vir.innerHTML
-        while(tela.value = "."){
-            vir.disabled = true
-        }
+         const ultimoNumero = tela.value.split(/[\+\-\*\/]/).pop(); // pega o último número após o operador
+
+    if (!ultimoNumero.includes(".")) {
+        tela.value += vir.innerText; // Adiciona o ponto
+    } else {
+        window.alert("Esse número já tem um ponto decimal.");
+    }
     }else{
         window.alert("Escolha um número primeiro")
     }
